cd56f41677b3600f5645accfb9a9a8258286a40d
TheVeryDarkness <3266343194@qq.com>
2024-02-05 00:17:02 +0800

    Removed my copyright claims.
    Just follow pest's licenses.

--- pest/src/typed/template/repetition.rs
+++ pest/src/typed/template/repetition.rs
@@ -1,12 +1,3 @@
-// pest-typed. A statically typed version of pest.
-// Copyright (c) 2023 黄博奕
-//
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. All files in the project carrying such notice may not be copied,
-// modified, or distributed except according to those terms.
-
 //! Predefined tree nodes generics.
 //! The generator may use this for convenience.
 //! Normally you don't need to reference this module by yourself.
--- pest/src/typed/tracker.rs
+++ pest/src/typed/tracker.rs
@@ -1,12 +1,3 @@
-// pest-typed. A statically typed version of pest.
-// Copyright (c) 2023 黄博奕
-//
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. All files in the project carrying such notice may not be copied,
-// modified, or distributed except according to those terms.
-
 //! Tracker for parsing failures.
 
 // Copied from [pest-typed/tracker.rs].
