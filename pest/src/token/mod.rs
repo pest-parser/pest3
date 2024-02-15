d2ec4efbdaf1d92383164ac15f719afa9dc2b35f
TheVeryDarkness <3266343194@qq.com>
2024-02-15 14:55:51 +0800

    Implement Display for Pair

--- pest/src/token/mod.rs
+++ pest/src/token/mod.rs
@@ -1,5 +1,7 @@
 //! Definition of tokens.
 
+use std::fmt::Display;
+
 /// Matched token. Not optimized.
 ///
 /// See [Token](pest2::Token) and [Pair](pest2::iterators::Pair).
@@ -9,4 +11,18 @@ pub struct Pair<R> {
     pub start: usize,
     pub end: usize,
     pub children: Vec<Self>,
+}
+impl<R: crate::RuleType> Display for Pair<R> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}({}, {}, [", self.rule, self.start, self.end,)?;
+        let mut iter = self.children.iter();
+        if let Some(first) = iter.next() {
+            write!(f, "{}", first)?;
+        }
+        for item in iter {
+            write!(f, ", {}", item)?;
+        }
+        write!(f, "])")?;
+        Ok(())
+    }
 }
