9cfbba919b43b698a0899b3dd5c1e6b69516af9c
TheVeryDarkness <3266343194@qq.com>
2024-02-20 19:53:54 +0800

    Update json example.

--- derive/tests/json.rs
+++ derive/tests/json.rs
@@ -5,19 +5,18 @@
 #[allow(missing_docs)]
 pub mod json {
     use pest_derive::Parser;
- 
+
     /// JSON parser.
     #[derive(Parser)]
     #[grammar = "tests/json.pest"]
     pub struct JsonParser;
-
-} 
+}
 
 #[cfg(test)]
 mod tests {
     use pest::typed::{PairContainer, TypedParser};
 
-    use crate::json;
+    use crate::{json, json::generics::*};
     #[test]
     fn json_basic_test() {
         let sample1 = "{\"key\": \"value\"}";
@@ -36,4 +35,23 @@ mod tests {
         assert_eq!(&sample1[value[0].start..value[0].end], "\"value\"");
     }
 
+    /// Test in structural style.
+    #[test]
+    fn json_structural_test() {
+        let sample1 = "{\"key\": \"value\"}";
+        let s1: json::rules::json = json::JsonParser::try_parse(sample1).unwrap();
+        let value: json::rules::value<'_> = s1.content.field_1;
+        match *value.content {
+            Choice6::Choice2(object) => {
+                let (_, mut pairs, _) = object.content.into_tuple();
+                assert_eq!(pairs.content.len(), 1);
+                let pair = pairs.content.pop().unwrap();
+                let (key, _, value) = pair.content.into_tuple();
+                let (_, key, _) = key.content.into_tuple();
+                assert_eq!(key.span.as_str(), "key");
+                assert_eq!(value.span.as_str(), "\"value\"");
+            }
+            _ => {}
+        }
+    }
 }
